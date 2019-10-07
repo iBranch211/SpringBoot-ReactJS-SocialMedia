b41390cc2670f2dab7761c36ac0a0b8072ff2504
iBranch211 <leo.esaki211@gmail.com>
2019-10-08 10:42:56 +1100

    move Home button to the left

--- frontend/app/test/specs/basic.js
+++ frontend/app/test/specs/basic.js
@@ -1,40 +1,9 @@
 const assert = require('assert');
 const path = require('path');
-let casual = require('casual');
 let window1 = null
 let window2 = null
-
-
-casual.define('user', function () {
-    return {
-        username: casual.username,
-        firstName: casual.first_name,
-        lastName: casual.last_name,
-        password: "aA123456",
-        sNo: "s" + casual.integer(from = "3000000", to = "5000000"),
-        sEmail: casual.email,
-        phoneNo: "040"+ casual.integer(from = "1000000", to ="9999999")
-    }
-})
-
-casual.define('user_2', function () {
-    return {
-        username: casual.username,
-        firstName: casual.first_name,
-        lastName: casual.last_name,
-        password: "aA123456",
-        sNo: "s" + casual.integer(from = "3000000", to = "5000000"),
-        sEmail: casual.email,
-        phoneNo: "040"+ casual.integer(from = "1000000", to ="9999999")
-    }
-})
-
-let user = casual.user;
-let user2 = casual.user_2;
-
 describe('Login and Register test', () => {
     it('It should register a test account', () => {
-
         browser.url('http://localhost:4200');
         // browser.fullscreenWindow();
         window1 = browser.getWindowHandle();
@@ -43,36 +12,28 @@ describe('Login and Register test', () => {
         registerBtn.click();
 
         const username = $('input[name="username"]');
-        // username.setValue("tester01");
-        username.setValue(user.username);
+        username.setValue("tester01");
 
         const password = $('input[name="password"]');
-        password.setValue(user.password);
-        // password.setValue(user.password);
+        password.setValue("aA123456");
 
         const retypepassword = $('input[name="retypepassword"]');
         retypepassword.setValue(password.getValue());
-        // retypepassword.setValue(password.getValue());
 
         const firstName = $('input[name="firstname"]');
-        // firstName.setValue("Tester");
-        firstName.setValue(user.firstName);
+        firstName.setValue("Tester");
 
         const lastName = $('input[name="lastname"]');
-        // lastName.setValue("Nguyen");
-        lastName.setValue(user.lastName);
+        lastName.setValue("Nguyen");
 
         const sNo = $('input[name="studentnumber"]');
-        // sNo.setValue("s9999000");
-        sNo.setValue(user.sNo);
+        sNo.setValue("s9999000");
 
         const sEmail = $('input[name="email"]');
-        // sEmail.setValue("s9999000@student.rmit.edu.au");
-        sEmail.setValue(user.sEmail);
+        sEmail.setValue("s9999000@student.rmit.edu.au");
 
         const phoneNo = $('input[name="phonenumber"]');
-        // phoneNo.setValue("0410999999");
-        phoneNo.setValue(user.phoneNo);
+        phoneNo.setValue("0410999999");
 
         const submitBtn = $('button[name="register"]');
         submitBtn.click();
@@ -84,21 +45,19 @@ describe('Login and Register test', () => {
 
     it('It should login with registered user', () => {
         const username = $('input[name="username"]');
-        username.setValue(user.username);
+        username.setValue("tester01");
 
         const password = $('input[name="password"]');
-        password.setValue(user.password);
+        password.setValue("aA123456");
 
         const loginBtn = $('button[name="login"]');
         loginBtn.click();
 
         browser.waitUntil(() => {
-            return browser.getUrl() === 'http://localhost:4200/welcome/' + user.username
+            return browser.getUrl() === 'http://localhost:4200/welcome/tester01'
         }, 3000);
-        // const logoutBtn = $('button[name="logout"]');
-        // logoutBtn.click();
     })
-});
+})
 
 describe('Post status and comment', () => {
     it('It should post status with logged in user', () => {
@@ -110,10 +69,10 @@ describe('Post status and comment', () => {
             statusInput.setValue("This is the test status number " + i);
             submitBtn.click();
         }
-    })
+    });
 
     it('It should comment on posted status', () => {
-        const comments = $$('input[placeholder="Write a comment.."]');
+        const comments = $$('input[placeholder="Write a comment.."');
         for (const key in comments) {
             if (comments.hasOwnProperty(key)) {
                 const comment = comments[key];
@@ -121,23 +80,22 @@ describe('Post status and comment', () => {
                 browser.keys("Enter");
             }
         }
-    })
+    });
 });
 
 describe('Change avatar and background', () => {
     it('It should change avatar', () => {
-        const profileLink = $('a[href="/profile/' + user.username + '"]')
-        // const profileLink = $('a[href="/profile/tester01"]');
+        const profileLink = $('a[href="/profile/tester01"]');
         profileLink.click();
 
         browser.waitUntil(() => {
             return !browser.isLoading();
-        }, 3000);
+        },3000);
 
         const avatarBtn = $('.image-cropper');
         avatarBtn.click();
 
-        const uploadAvatarBtn = $('input[name="avatarfile"]');
+        const uploadAvatarBtn = $('input[name="avatarfile"]')
         const filePath = path.join(__dirname, '/assets/test1-ava.jpg');
         uploadAvatarBtn.setValue(filePath);
 
@@ -149,72 +107,54 @@ describe('Change avatar and background', () => {
     it('It should change banner', () => {
         browser.waitUntil(() => {
             return !browser.isLoading();
-        }, 3000);
+        },3000);
 
         const bannerBtn = $('.banner');
         bannerBtn.click();
 
-        const uploadBannerBtn = $('input[name="backgroundfile"]');
+        const uploadBannerBtn = $('input[name="backgroundfile"]')
         const filePath = path.join(__dirname, '/assets/test1-banner.jpg');
 
         uploadBannerBtn.setValue(filePath);
         const saveBtn = $('.btn-primary=Save Changes');
         saveBtn.click();
         browser.pause(3000);
-    })
-
-});
+    });
+})
 
 // -----------------------------------------------------------------------------
 
 describe('Create an account for second test user', () => {
-
-    it('It should open a new window', () => {
-            browser.url('http://localhost:4200');
-            browser.newWindow('http://localhost:4200');
-            // browser.switchWindow('http://localhost:4200/login');
-            window2 = browser.getWindowHandle();
-            // browser.newWindow(window2);
-    })
-
     it('It should register a second test account', () => {
+        browser.newWindow('http://localhost:4200');
+        window2 = browser.getWindowHandle();
 
-        const logoutBtn = $('a[name="logout"]');
-        logoutBtn.click();
-    
         const registerBtn = $('button[name="register"]');
         registerBtn.click();
 
         const username = $('input[name="username"]');
-        // username.setValue("tester02");
-        username.setValue(user2.username);
+        username.setValue("tester02");
 
         const password = $('input[name="password"]');
-        // password.setValue("aA123456");
-        password.setValue(user2.password);
+        password.setValue("aA123456");
 
         const retypepassword = $('input[name="retypepassword"]');
         retypepassword.setValue(password.getValue());
 
         const firstName = $('input[name="firstname"]');
-        // firstName.setValue("Tester");
-        firstName.setValue(user2.firstName);
+        firstName.setValue("Tester");
 
         const lastName = $('input[name="lastname"]');
-        // lastName.setValue("Nguyen");
-        lastName.setValue(user2.lastName);
+        lastName.setValue("Pham");
 
         const sNo = $('input[name="studentnumber"]');
-        // sNo.setValue("s9999000");
-        sNo.setValue(user2.sNo);
+        sNo.setValue("s9999001");
 
         const sEmail = $('input[name="email"]');
-        // sEmail.setValue("s9999000@student.rmit.edu.au");
-        sEmail.setValue(user2.sEmail);
+        sEmail.setValue("s9999001@student.rmit.edu.au");
 
         const phoneNo = $('input[name="phonenumber"]');
-        // phoneNo.setValue("0410999999");
-        phoneNo.setValue(user2.phoneNo);
+        phoneNo.setValue("0410999333");
 
         const submitBtn = $('button[name="register"]');
         submitBtn.click();
@@ -226,13 +166,11 @@ describe('Create an account for second test user', () => {
     })
 
     it('It should login with the second registered user', () => {
-
-        browser.pause(500);
         const username = $('input[name="username"]');
-        username.setValue(user2.username);
+        username.setValue("tester02");
 
         const password = $('input[name="password"]');
-        password.setValue(user2.password);
+        password.setValue("aA123456");
 
         const loginBtn = $('button[name="login"]');
         loginBtn.click();
@@ -251,10 +189,10 @@ describe('Post status and comment for second user', () => {
             statusInput.setValue("This is the test status number " + i + " for second user");
             submitBtn.click();
         }
-    })
+    });
 
     it('It should comment on posted status', () => {
-        const comments = $$('input[placeholder="Write a comment.."]');
+        const comments = $$('input[placeholder="Write a comment.."');
         for (const key in comments) {
             if (comments.hasOwnProperty(key)) {
                 const comment = comments[key];
@@ -262,13 +200,13 @@ describe('Post status and comment for second user', () => {
                 browser.keys("Enter");
             }
         }
-    })
+    });
 });
 
 
 describe('Message PM system', () => {
     it('It should beable to send Private Message', () => {
-        const selectChat = $('.chat-username=' + user.username);
+        const selectChat = $('.chat-username=tester01');
         selectChat.click();
 
         const chatInput = $('.chatControl input');
@@ -284,13 +222,13 @@ describe('Message PM system', () => {
         browser.switchToWindow(window2);
         chatInput.setValue("Pretty good, thanks.");
         browser.keys("Enter");
-    })
+    });
 });
 
 
 describe('Change avatar and background of the second tester', () => {
     it('It should change avatar of second tester', () => {
-        const profileLink = $('a[href="/profile/' + user2.username + '"]');
+        const profileLink = $('a[href="/profile/tester02"]');
         profileLink.click();
 
         browser.waitUntil(() => {
@@ -317,12 +255,12 @@ describe('Change avatar and background of the second tester', () => {
         const bannerBtn = $('.banner');
         bannerBtn.click();
 
-        const uploadBannerBtn = $('input[name="backgroundfile"]');
+        const uploadBannerBtn = $('input[name="backgroundfile"]')
         const filePath = path.join(__dirname, '/assets/test2-banner.jpg');
 
         uploadBannerBtn.setValue(filePath);
         const saveBtn = $('.btn-primary=Save Changes');
         saveBtn.click();
         browser.pause(3000);
-    })
-});
+    });
+})
