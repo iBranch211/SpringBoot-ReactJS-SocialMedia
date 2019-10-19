f76c716a5add3f89fdd03a94bc64dba336204da0
iBranch211 <leo.esaki211@gmail.com>
2019-10-19 22:41:25 +1100

    Hopefully it works

--- restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/profiletest/PostAndComment.java
+++ restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/profiletest/PostAndComment.java
@@ -42,6 +42,8 @@ public class PostAndComment {
 
 	@Before
 	public void setUp() throws Exception {
+		RestAssured.baseURI = "https://localhost";
+		RestAssured.port = 8080;
 		
 		newProfile1.setUsername("lam");
 		newProfile1.setFirstname("Lam");
