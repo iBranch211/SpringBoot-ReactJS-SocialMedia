d12d653acda03bcd47c7c1da371dfe5b6dbc9bcb
iBranch211 <leo.esaki211@gmail.com>
2019-10-19 20:06:11 +1100

    Profile and user methods now throw exceptions

--- frontend/app/wdio.conf.js
+++ frontend/app/wdio.conf.js
@@ -59,9 +59,6 @@ exports.config = {
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
-        'goog:chromeOptions': {
-            args: ['headless', 'disable-gpu']
-        }
     }],
     //
     // ===================
